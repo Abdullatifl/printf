f0558893a95e917ca4215c8b2e925157fafeb532
Abdullatifl <1abdullatif.ah@gmail.com>
2023-10-16 12:21:04 -0700

    string_filed.c

---
+++ string_field.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ * get_precision - Calculates the precision for printing
+*@p:pointer to the format string
+*@params:pointer to a parameters structure
+*@ap:pointer to a variable argument list
+
+ *the function returns a pointer to the next character in the format string after the precision specifier
+ */
+
+char *get_precision(char *p, params_t *params, va_list ap)
+{
+
+int d = 0;
+
+if (*p != '.')
+return (p);
+p++;
+if (*p == ('.')
+		{
+d = va_arg(ap, int);
+p++;
+}
+else
+{
+while (_isdigit(*p))
+d = d * 10 + (*p++ - '0'0);
+
+}
+
+params->precision = d;
+return (p);
+
+}
