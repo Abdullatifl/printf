b24d712ad8683f42ebf1072c6820a10f66f6ca52
nashwa <n7216069@example.com>
2023-10-17 11:01:36 -0700

    a

--- string_field.c
+++ string_field.c
@@ -11,22 +11,21 @@
 
 char *get_precision(char *p, params_t *params, va_list ap)
 {
-	int d = 0;
+	int x = 0;
 
-		if (*p != '.')
-			return (p);
-
-		p++;
-		if (*p == '*')
-		{
-			d = va_arg(ap, int);
-			p++;
-		}
-		else
-		{
-			while (_isdigit(*p))
-			d = d * 10 + (*p++ - '0');
-		}
-		params->precision = d;
+	if (*p != '.')
 		return (p);
+	p++;
+	if (*p == '*')
+	{
+		x = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
+		while (_isdigit(*p))
+			x = x * 10 + (*p++ - '0');
+	}
+	params->precision = x;
+	return (p);
 }
